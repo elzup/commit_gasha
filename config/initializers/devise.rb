203342015bb413e6e05d5840cb2e92555bf42dfb
elzzup <guild0105@gmail.com>
2015-10-17 03:25:52 +0900

    Create oauth controllers

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,17 @@
 Devise.setup do |config|
+  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  require 'devise/orm/active_record'
   OAUTH_CONFIG = YAML.load_file("#{Rails.root}/config/omniauth.yml")[Rails.env].symbolize_keys!
+
+  config.case_insensitive_keys = [:email]
+  config.strip_whitespace_keys = [:email]
+  config.skip_session_storage = [:http_auth]
+  config.stretches = Rails.env.test? ? 1 : 10
+  config.reconfirmable = true
+  config.expire_all_remember_me_on_sign_out = true
+  config.password_length = 8..72
+  config.reset_password_within = 6.hours
+  config.sign_out_via = :delete
 
   # https://github.com/intridea/omniauth-github
   # http://developer.github.com/v3/oauth/
