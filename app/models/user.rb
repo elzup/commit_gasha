0bf14b4cf66b4ba740088be4e5fe2e22174eb9a3
elzzup <guild0105@gmail.com>
2015-10-22 23:11:09 +0900

    create commits methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,27 @@
 class User < ActiveRecord::Base
 
   validates :name, presence: true
+
+  def commits
+    @commits ||= GithubClient.commits(username)
+  end
+
+  def set_gitinfo(github)
+    @github = github
+  end
+
+  def username
+    @username ||= @github['login']
+  end
+
+  def self.login_user(token)
+    # ログインしているユーザを返す, セッションがなければnil
+    if token.nil?
+      return nil
+    end
+    user_info = GithubClient.user(token)
+    user = User.find_or_create_by(github_id: user_info['id'])
+    user.set_gitinfo(user_info)
+    user
+  end
 end
